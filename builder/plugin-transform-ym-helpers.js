667637289231a07ed5c9d9e251cf8b05e994a483
flapenguin <flapenguin@users.noreply.github.com>
2017-11-15 22:49:58 +0300

    use babel to rewrite native promises to vow

--- builder/plugin-transform-ym-helpers.js
+++ builder/plugin-transform-ym-helpers.js
@@ -1,39 +1,44 @@
-/* jshint esversion: 6 */
+module.exports = function ({ template, types: t }) {
+    function replaceGlobals(path, file) {
+        // Object.assign => Object
+        // Promise => Promise
+        const variableName =
+            path.isMemberExpression() && path.get('object').isIdentifier() ? path.get('object').node.name :
+            path.isIdentifier() ? path.node.name :
+            null;
 
-module.exports = function (
-    /* eslint-disable */
-    { template, types: t }
-    /* eslint-enable */) {
-    function onCallExpression(path, file) {
-        const calleePath = path.get('callee');
-
-        const pattern = Object.keys(file.opts.replace || {}).find(x => calleePath.matchesPattern(x));
-        if (!pattern) {
+        if (!variableName ||
+            !path.scope.hasGlobal(variableName) ||
+            path.scope.hasOwnBinding(variableName)) {
             return;
         }
 
-        const replacement = file.opts.replace[pattern];
-        const simple = typeof replacement === 'string';
-        const module = simple ? replacement : replacement[0];
+        const replacement = findReplacement(path, file.opts.replaceGlobals);
+        if (!replacement) {
+            return;
+        }
 
         try {
-            const helper = this.file.importYmModule(module);
-            path.node.callee = simple
-                ? helper
-                : t.memberExpression(helper, t.identifier(replacement[1]));
+            const simple = typeof replacement === 'string';
+            const helper = this.file.importYmModule(simple ? replacement : replacement[0]);
+
+            const replacementNode = simple ? helper :
+                t.memberExpression(helper, t.identifier(replacement[1]));
+
+            path.replaceWith(replacementNode);
         } catch (e) {
             console.error(`Error while replacing ${pattern} with ${replacement.join('.')}:`, e);
         }
     }
 
     function onProgramEnter(path, state) {
-        const helpersOverrides = state.opts.helpersOverrides || {};
+        const overrideHelpers = state.opts.overrideHelpers || {};
 
         // HACK: monkey-patch addHelper to add our specific helpers, instead of babel ones.
         const addHelper = state.file.addHelper;
         state.file.addHelper = function (name) {
-            if (helpersOverrides[name]) {
-                return state.file.importYmModule(helpersOverrides[name]);
+            if (overrideHelpers[name]) {
+                return this.importYmModule(overrideHelpers[name]);
             }
 
             return addHelper.call(this, name);
@@ -43,7 +48,23 @@ module.exports = function (
     return {
         visitor: {
             Program: { enter: onProgramEnter },
-            CallExpression: onCallExpression
+            ReferencedIdentifier: replaceGlobals,
+            CallExpression(path, file) {
+                replaceGlobals.call(this, path.get('callee'), file);
+            },
         }
     };
 };
+
+function findReplacement(path, replacements) {
+    if (path.isIdentifier()) {
+        return replacements[path.node.name];
+    }
+
+    if (path.isMemberExpression()) {
+        const key = Object.keys(replacements).find(x => path.matchesPattern(x));
+        return key && replacements[key];
+    }
+
+    throw new Error(`Can't find replacement. Expected Identifier or MemberExpression, got ${path.node.type}.`);
+}
